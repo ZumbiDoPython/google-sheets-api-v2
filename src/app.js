68033c2ab3ffd637bec2b77d7579dea5991e9f3b
Pedro Mourão <pepel.smourao@gmail.com>
2025-01-08 23:41:06 -0300

    cors adjust

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sheetRoutes = require('./routes/sheetRoutes');
+const sheetRoutes = require('./routes/sheetRoutes');const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
+
 
 app.use(bodyParser.json());
 app.use('/api', sheetRoutes);
